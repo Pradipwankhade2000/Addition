6234e98994a49ed5de93700b09d8d49ef01be662
Pradip Wankhade <pwankhade220@gmail.com>
2023-05-31 15:26:59 +0530

    this the AdditionEx

---
+++ src/com/java/AdditionEx.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class AdditionEx {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		int result = num1 + num2 ;
+		
+		System.out.println("the addition of two numbers : " + result);
+
+	}
+
+}
